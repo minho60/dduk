c4b0dfa42939b5d2853746b998dc497143f00143
minho60 <minho031@gmail.com>
2026-05-22 14:39:47 +0900

    Refactor: remove redundant root src folder after merge

--- src/main/java/com/dduk/dto/inventory/PurchaseOrderStatusUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package com.dduk.dto.inventory;
-
-import lombok.Getter;
-
-@Getter
-public class PurchaseOrderStatusUpdateDto {
-
-    private String status;
-}
--- src/main/java/com/dduk/dto/inventory/VendorStatusUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package com.dduk.dto.inventory;
-
-import lombok.Getter;
-
-@Getter
-public class VendorStatusUpdateDto {
-
-    private String status;
-}
