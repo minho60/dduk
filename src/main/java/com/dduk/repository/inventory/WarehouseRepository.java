90a0adcabec8349c2ecb9ad73c55191eb1a98763
minho60 <minho031@gmail.com>
2026-05-22 16:47:33 +0900

    Refactor: remove redundant root src directory in develop branch

--- src/main/java/com/dduk/repository/inventory/WarehouseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dduk.repository.inventory;
-
-import com.dduk.entity.inventory.Warehouse;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-}
