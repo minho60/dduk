18a7f5f387eb71456337f6f52049ac3bd4795c2a
410097800525331 <xxx696193@gmail.com>
2026-06-05 15:28:36 +0900

    사이드바 업체명 우측 아이콘 삭제

--- frontend/services/common/sidebar-menu.js
+++ frontend/services/common/sidebar-menu.js
@@ -154,7 +154,6 @@
                     </div>
                     <div class="dduk-inline-005">
                         <span data-template-id="workspace-name" class="canva-text dduk-inline-006">(주)아망티</span>
-                        <i class="dduk-inline-007" data-lucide="chevron-down"></i>
                     </div>
                     <div class="dduk-inline-008">
                         <a class="sidebar_icon_btn" aria-label="AI" href="#" id="quickBtnAI"><i class="dduk-inline-009" data-lucide="bot"></i></a>
